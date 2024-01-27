a6b05ff0398e6e498970cccee3609703b843124e
Gultekin <gultekincirik@gmail.com>
2024-01-27 02:31:12 +0100

    first commit

---
+++ get_weather_api.js
@@ -0,0 +1,48 @@
+const apiKey = "274dbf21f62b0e5b8a8f4c43e06a8bff";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    // log invalid city name error
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+    } else{
+
+        var data = await response.json();
+
+        // log the weather information
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        // change the weather icon
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
+
+        // hide invalid city error
+        document.querySelector(".error").style.display = "none";
+
+    }
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+
